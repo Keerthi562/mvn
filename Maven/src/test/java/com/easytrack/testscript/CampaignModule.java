a69010208f92c3c12bf86702f1e6931b0aace29a
QSP <QSP@DESKTOP-3JRUFPN>
2025-03-12 10:42:42 +0530

    Added selenium code

--- Maven/src/test/java/com/easytrack/testscript/CampaignModule.java
+++ Maven/src/test/java/com/easytrack/testscript/CampaignModule.java
@@ -1,13 +1,21 @@
 package com.easytrack.testscript;
 
+import java.time.Duration;
+
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class CampaignModule {
 	@Test
 public void demo() {
-		Reporter.log("Campaign has been created",true);
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://qspiders.com");
+		Reporter.log(driver.getTitle(),true);
+		driver.quit();
 	}
 }
